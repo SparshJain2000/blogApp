c6ebcf6d5b5bfbd3d168062d8497757a3978aec5
SparshJain2000 <jainsparsh0801@gmail.com>
2020-11-08 01:45:14 +0530

    add memory store

--- blogApp-backend/server.js
+++ blogApp-backend/server.js
@@ -1,60 +1,60 @@
 const express = require("express"),
-  app = express(),
-  cors = require("cors"),
-  User = require("./models/user.model"),
-  mongoose = require("mongoose"),
-  passport = require("passport"),
-  LocalStratergy = require("passport-local"),
-  blogsRouter = require("./routes/blogs.router"),
-  userRouter = require("./routes/user.router"),
-  bodyParser = require("body-parser");
+    app = express(),
+    cors = require("cors"),
+    User = require("./models/user.model"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStratergy = require("passport-local"),
+    blogsRouter = require("./routes/blogs.router"),
+    userRouter = require("./routes/user.router"),
+    bodyParser = require("body-parser");
 require("dotenv").config();
-var session = require('express-session')
-var MemoryStore = require('memorystore')(session)
- 
+var session = require("express-session");
+var MemoryStore = require("memorystore")(session);
+
 //==========================================================================
 app.use(bodyParser.json());
 app.use(cors());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept",
-  );
-  next();
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept",
+    );
+    next();
 });
-app.use(session(
-  {
-      secret: process.env.SECRET,
-      cookie: { maxAge: 86400000 },
-      store: new MemoryStore({
-        checkPeriod: 86400000 // prune expired entries every 24h
-      }),
-      resave: false,
-      saveUninitialized: false,
-  })
+app.use(
+    session({
+        secret: process.env.SECRET,
+        cookie: { maxAge: 86400000 },
+        store: new MemoryStore({
+            checkPeriod: 86400000, // prune expired entries every 24h
+        }),
+        resave: false,
+        saveUninitialized: false,
+    }),
 );
 app.use((req, res, next) => {
-  res.locals.currentUser = req.user;
-  next();
+    res.locals.currentUser = req.user;
+    next();
 });
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(
-  new LocalStratergy({ usernameField: "username" }, User.authenticate()),
+    new LocalStratergy({ usernameField: "username" }, User.authenticate()),
 );
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 //===========================================================================
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 });
 mongoose.connection.once("open", () => {
-  console.log("connected to MONGO");
+    console.log("connected to MONGO");
 });
 //===========================================================================
 app.use("/api/blogs", blogsRouter);
@@ -62,5 +62,5 @@ app.use("/api/users", userRouter);
 
 const port = 8080 || process.env.PORT;
 app.listen(port, () => {
-  console.log(`Listening to ${port}`);
+    console.log(`Listening to ${port}`);
 });
